4602b974e7b9a73fed608ec8570e4651996d5314
MCeciliaLuna <lunama.cecilia@gmail.com>
2022-09-20 16:43:42 -0300

    nodemon cambiado a node

--- controllers/login.js
+++ controllers/login.js
@@ -1,18 +1,34 @@
 const bcrypt = require('bcrypt');
-const User = require('../models/users')
+const User = require('../models/users');
+const jwt = require('jsonwebtoken');
 
 const login = async(req,res) => {
   const { email, password } = req.body;
 
   const user = await User.findOne({ email })
 
-  // const result = bcrypt.compareSync(password)
-
-    console.log(user)
-  
-    res.json({
-      message: "Login concretado"
+  if(!user){
+    return res.status(404).json({
+      message: "usuario inexistente"
     })
+  }
+
+  const token = jwt.sign({ user }, 'comision24i', { expiresIn: '1h' })
+
+  const match = bcrypt.compareSync(password, user.password)
+
+  if (match) {
+    return res.status(200).json({
+      message: "usuario logueado correctamente",
+      token
+    })
+  } else {
+   return res.status(404).json({
+    message: "usuario o contraseña incorrectos"
+   }) 
+  }
+
+  
 }
 
 module.exports = { login }
